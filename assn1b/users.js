794c7f399b7c677432844b216163d2d5be16318f
RushiDT <124486843+RushiDT@users.noreply.github.com>
2025-03-20 09:55:07 +0530

    users.js

--- assn1b/users.js
+++ assn1b/users.js
@@ -1,12 +1,8 @@
-// users.js
+
 document.addEventListener("DOMContentLoaded", function() {
-    // Get user data from local storage
     const users = JSON.parse(localStorage.getItem('users')) || [];
-
-    // Get the userList table body
     const userList = document.getElementById('userList');
-
-    // Populate the table with user data
+    
     users.forEach(user => {
         const row = document.createElement('tr');
         row.innerHTML = `
